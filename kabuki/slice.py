6364014e87237ca9aa9d44b37af933d680ee5992
Imri Sofer <imrisofer@gmail.com>
2012-01-12 09:10:09 -0500

    cosmetic changes

--- kabuki/slice.py
+++ kabuki/slice.py
@@ -3,11 +3,17 @@ import pymc as pm
 rand = np.random.rand
 
 class SliceSampling(pm.StepMethod):
-    def __init__(self, stochastic, width, maxiter = 200, low_bound = None, up_bound = None, verbose=None, tally=True,):
-        
+    """
+    simple slice sampler
+    """
+    def __init__(self, stochastic, width = 0.5, maxiter = 200, verbose=None, tally=True,):
+        """
+        Input:
+            stochistic - stochastic node
+            width - the initial width of the interval
+            maxiter - maximum number of iteration allowed for stepping-out and shrinking
+        """
         pm.StepMethod.__init__(self, [stochastic], tally=tally)
-        self.low_bound = low_bound
-        self.up_bound = up_bound
         self.width = width
         self.neval = 0
         self.maxiter = maxiter
@@ -20,14 +26,13 @@ class SliceSampling(pm.StepMethod):
     def step(self):
         stoch = self.stochastic
         value = stoch.value
-        
+
         #sample vertical level
         z = self.logp_plus_loglike - np.random.exponential()
-        
+
         if self.verbose>2:
             print self._id + ' current value: %.3f' % value
             print self._id + ' sampled vertical level ' + `z`
-
 
         #position an interval at random starting position around the current value
         r = self.width * np.random.rand()
@@ -36,8 +41,6 @@ class SliceSampling(pm.StepMethod):
 
         if self.verbose>2:
             print 'initial interval [%.3f, %.3f]' % (xl, xr)
-        
-
 
         #step out to the left
         iter = 0
@@ -47,9 +50,8 @@ class SliceSampling(pm.StepMethod):
             stoch.value = xl
             iter += 1
 
-        assert iter < self.maxiter, "The step-out procedure failed"
+        assert iter < self.maxiter, "Step-out procedure failed"
         self.neval += iter
-
         if self.verbose>2:
             print 'after %d iteration interval is [%.3f, %.3f]' % (iter, xl, xr)
 
@@ -61,19 +63,16 @@ class SliceSampling(pm.StepMethod):
             stoch.value = xr
             iter += 1
 
-        assert iter < self.maxiter, "The step-out procedure failed"
+        assert iter < self.maxiter, "Step-out procedure failed"
         self.neval += iter
-
         if self.verbose>2:
             print 'after %d iteration interval is [%.3f, %.3f]' % (iter, xl, xr)
 
-
-        #A new point is found by picking uniformly from the interval [xl, xr].
+        #draw a new point from the interval [xl, xr].
         xp = rand()*(xr-xl) + xl
         stoch.value = xp
 
-        #shrink the interval (or hyper-rectangle) if a point outside the
-        #density is drawn.
+        #if the point is outside the interval than shrink it and draw again
         iter = 0
         while(self.logp_plus_loglike < z) and (iter < self.maxiter):
             if (xp > value):
@@ -84,7 +83,7 @@ class SliceSampling(pm.StepMethod):
             stoch.value = xp
             iter += 1
 
-        assert iter < self.maxiter, "The shrink-in procedure failed."
+        assert iter < self.maxiter, "Shrink-in procedure failed."
         self.neval += iter
         if self.verbose>2:
             print 'after %d iteration found new value: %.3f' % (iter, xp)
